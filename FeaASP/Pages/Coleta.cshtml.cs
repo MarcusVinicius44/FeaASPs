0c416f4d7a9aa5a3b93e4834991e30906a5afeff
021.900244 <021.900244@C305-237-028>
2024-09-06 15:11:18 -0300

    Adicionar arquivos de projeto.

---
+++ FeaASP/Pages/Coleta.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FEAasp.Pages
+{
+    public class ColetaModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ColetaModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ FeaASP/Pages/Contato.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FEAasp.Pages
+{
+    public class ContatoModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ContatoModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
